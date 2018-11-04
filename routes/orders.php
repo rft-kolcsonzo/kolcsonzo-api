b0d2a59b986ffd17ccfcfd9f0265bc67907871eb
Ilyés Imre <emericodesign@me.com>
2018-11-04 23:53:34 +0100

    Orders CRUD

--- routes/orders.php
+++ routes/orders.php
@@ -4,28 +4,55 @@ $app->group('/orders', function (){
     // GET /orders
     $this->get('', function ($request, $response) {
         $session = $request->getAttribute('session');
-
+        
         $orders = $this->orderModel->getAll();
-        //ide jon ha valami szempont szerint keresunk
-
+        
         return $response->withJson($orders);
     });
 
     // GET /orders/{orderId}
     $this->get('/{orderId}', function ($request, $response, $args) {
-        $orderId = $args['orderId'];
+        $id = $args['orderId'];
 
         if (!$session = $request->getAttribute('session')) {
             return $response->withJson([ 'message' => 'Csak aktív munkafolyamatban érhető el ez a metódus!' ], 412);
         }
 
-        if ($order = $this->orderModel->get($orderId)) {
+        if ($order = $this->orderModel->getOrderById($id)) {
 
-            //ide jon ha valami szempont szerint keresunk
-            
             return $response->withJson($order);
         }
 
         return $response->withJson([ 'message' => 'Nem létezik ilyen rendelés!' ], 404);
     });
+    
+    $this->post('/insert', function ($request, $response) {
+        
+        /*if (!$session = $request->getAttribute('session')) {
+            return $response->withJson([ 'message' => 'Csak aktív munkafolyamatban érhető el ez a metódus!' ], 412);
+        }*/
+
+        $datas = $request->getParsedBody();
+        $response = $this->orderModel->insertOrder($datas);
+        return $this->response->withJson(['message' => $response]);
+    });
+
+    $this->delete('/[{orderId}]', function ($request, $response, $args) {
+        $id = $args['orderId'];
+
+        if ($this->orderModel->deleteOrder($id)) {
+            return $this->response->withJson(['message' => 'Sikeres törlés']);
+        }
+
+        return $this->response->withJson(['message' => 'Hiba történt']);
+    }); 
+
+    $this->put('/[{orderId}]', function ($request, $response, $args) {
+        $datas = $request->getParsedBody();
+        $id = $args['orderId'];
+        $response = $this->orderModel->updateOrder($id, $datas);
+        return $this->response->withJson(['message' => $response]);
+    });
+    
+    
 })->add($SessionMiddleware); // Use SessionMiddleware

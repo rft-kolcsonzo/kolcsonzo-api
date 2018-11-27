3dd798e99b94720a760f34820e3fecce5a3ae343
Józsa Péter <jozsa.peter@pwstudio.hu>
2018-11-27 18:49:30 +0100

    Dockeresítés

--- config.php
+++ config.php
@@ -3,7 +3,16 @@
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
-$config['db']['host'] = 'localhost';
-$config['db']['user'] = 'root';
-$config['db']['pass'] = '';
-$config['db']['dbname'] = 'car_rental';
+$inDocker = getenv('RUNTIME') === 'DOCKER';
+
+if ($inDocker) {
+    $config['db']['host'] = getenv('DB_HOST');
+    $config['db']['user'] = getenv('DB_USER');
+    $config['db']['pass'] = getenv('DB_PASSWORD');
+    $config['db']['dbname'] = getenv('DB_NAME');
+} else {
+    $config['db']['host'] = 'localhost';
+    $config['db']['user'] = 'root';
+    $config['db']['pass'] = '';
+    $config['db']['dbname'] = 'car_rental';
+}
